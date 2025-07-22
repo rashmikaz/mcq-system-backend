4ff1d8c606dd987cce4d3ee23f1a41816c8162a7
rashmikaz <phrashmika@gmail.com>
2025-07-23 00:40:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require("express");
+const app = express();
+
+console.log("Hello World");
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+});
+
+app.listen(3000, () => {
+    console.log("Express server running at http://localhost:3000");
+});
